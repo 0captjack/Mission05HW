73e91808195186def9b1f2c843f3563a622974bc
0captjack <85979485+0captjack@users.noreply.github.com>
2022-01-30 12:54:24 -0700

    Add project files.

---
+++ Mission04HW/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission04HW.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04HW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext _instanceContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext someName)
+        {
+            _logger = logger;
+            _instanceContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(ApplicationResponse ar)
+        {
+            _instanceContext.Add(ar);
+            _instanceContext.SaveChanges();
+            return View("Index", ar);
+        }
+
+        public IActionResult Bacon()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission04HW/Models/MovieContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04HW.Models
+{
+    public class MovieContext :DbContext
+    {
+        public MovieContext (DbContextOptions <MovieContext> options) : base (options)
+        {
+            //lEbLaNc
+        }    
+        public DbSet<ApplicationResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //seeding the batadase
+            mb.Entity<ApplicationResponse>().HasData(
+                new ApplicationResponse
+                {
+                    ApplicationId = 1,
+                    Category = "Sci-Fi",
+                    Title = "Sunshine",
+                    Year = "2007",
+                    Director = "Danny Boyle",
+                    Rating = "R",
+                    Edited = false,
+                    Lent_To = "",
+                    Notes = "This movie is incredible"
+                },
+                new ApplicationResponse
+                {
+                    ApplicationId = 2,
+                    Category = "Drama",
+                    Title = "Prisoners",
+                    Year = "2013",
+                    Director = "Denis Villeneuve",
+                    Rating = "R",
+                    Edited = false,
+                    Lent_To = "",
+                    Notes = "Excellent"
+                },
+                new ApplicationResponse
+                {
+                    ApplicationId = 3,
+                    Category = "Thriller",
+                    Title = "Jaws",
+                    Year = "1975",
+                    Director = "Steven Spielberg",
+                    Rating = "PG",
+                    Edited = false,
+                    Lent_To = "",
+                    Notes = "Paved the way for so many great films"
+                }
+            );
+        }
+    }
+}
